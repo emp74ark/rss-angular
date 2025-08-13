92ee6b94b8887e974f6024f88400e538a32d66a3
Andrei Yurkouski <emp74ark@gmail.com>
2025-08-13 12:35:07 +0200

    refactor(home): extract article list to reusable component

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -4,39 +4,31 @@ import { FeedService } from '../../services/feed-service'
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { HttpErrorResponse } from '@angular/common/http'
 import { catchError, of } from 'rxjs'
-import { DatePipe } from '@angular/common'
 import { MatButton, MatIconButton } from '@angular/material/button'
-import { MatProgressBar } from '@angular/material/progress-bar'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonToggleModule } from '@angular/material/button-toggle'
 import { MatIconModule } from '@angular/material/icon'
 import { RowSpacer } from '../../components/row-spacer/row-spacer'
-import { Router, RouterLink } from '@angular/router'
+import { Router } from '@angular/router'
 import { Article } from '../../entities/article/article.types'
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator'
 import { TagService } from '../../services/tag-service'
 import { Tag } from '../../entities/tag/tag.types'
-import { MatChipOption, MatChipSet } from '@angular/material/chips'
 import { TitleService } from '../../services/title-service'
-import { SafeHtmlPipe } from '../../pipes/safe-html-pipe'
+import { ArticleList } from '../../components/article-list/article-list'
 
 @Component({
   selector: 'app-home',
   imports: [
     MatCardModule,
-    DatePipe,
-    MatProgressBar,
     MatToolbarModule,
     MatButtonToggleModule,
     MatIconModule,
     RowSpacer,
     MatIconButton,
-    RouterLink,
     MatPaginatorModule,
-    MatChipOption,
-    MatChipSet,
     MatButton,
-    SafeHtmlPipe,
+    ArticleList,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
@@ -134,41 +126,6 @@ export class HomePage implements OnInit {
     this.display.set(display)
   }
 
-  async onArticleClick(article: Article) {
-    await this.router.navigate(['subscription', article.subscriptionId, 'article', article._id])
-  }
-
-  markAsRead(article: Article, event: MouseEvent) {
-    event.stopPropagation()
-    if (article) {
-      this.feedService
-        .changeOneArticle({
-          articleId: article._id,
-          article: {
-            read: !article.read,
-          },
-        })
-        .pipe(
-          takeUntilDestroyed(this.destroyRef),
-          catchError((error: HttpErrorResponse) => {
-            console.log(error)
-            return of(null)
-          }),
-        )
-        .subscribe((result) => {
-          if (result === null) {
-            return
-          }
-          this.articles.update((prev) => {
-            if (prev !== null) {
-              return prev.map((a) => (a._id === result._id ? { ...a, read: !a.read } : a))
-            }
-            return prev
-          })
-        })
-    }
-  }
-
   markManyAsRead({
     articleIds,
     read,
@@ -196,40 +153,6 @@ export class HomePage implements OnInit {
       })
   }
 
-  onAddToBookmarks(article: Article, event: MouseEvent) {
-    const existingTag = article.tags.find((t) => t === this.favTagId())
-    const tags = existingTag
-      ? [...article.tags].filter((t) => t !== this.favTagId())
-      : [...article.tags, this.favTagId()]
-
-    event.stopPropagation()
-    if (article) {
-      this.feedService
-        .changeOneArticle({
-          articleId: article._id,
-          article: { tags },
-        })
-        .pipe(
-          takeUntilDestroyed(this.destroyRef),
-          catchError((error: HttpErrorResponse) => {
-            console.log(error)
-            return of(null)
-          }),
-        )
-        .subscribe((result) => {
-          if (result === null) {
-            return
-          }
-          this.articles.update((prev) => {
-            if (prev !== null) {
-              return prev.map((a) => (a._id === result._id ? { ...a, tags } : a))
-            }
-            return prev
-          })
-        })
-    }
-  }
-
   paginationHandler(event: PageEvent) {
     this.currentPage.set(event.pageIndex + 1)
     this.pageSize.set(event.pageSize)
@@ -244,40 +167,6 @@ export class HomePage implements OnInit {
       this.favFilter.update((prev) => !prev)
     }
     this.getData()
-  }
-
-  tagHandler(article: Article, tag: Tag, event: MouseEvent) {
-    const existingTag = article.tags.find((t) => t === tag._id)
-    const tags = existingTag
-      ? [...article.tags].filter((t) => t !== tag._id)
-      : [...article.tags, tag._id]
-
-    event.stopPropagation()
-    if (article) {
-      this.feedService
-        .changeOneArticle({
-          articleId: article._id,
-          article: { tags },
-        })
-        .pipe(
-          takeUntilDestroyed(this.destroyRef),
-          catchError((error: HttpErrorResponse) => {
-            console.log(error)
-            return of(null)
-          }),
-        )
-        .subscribe((result) => {
-          if (result === null) {
-            return
-          }
-          this.articles.update((prev) => {
-            if (prev !== null) {
-              return prev.map((a) => (a._id === result._id ? { ...a, tags } : a))
-            }
-            return prev
-          })
-        })
-    }
   }
 
   onRefreshAll() {
