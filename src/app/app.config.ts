a289e4d62838a768acc496fef860274c851f49c9
Andrei Yurkouski <emp74ark@gmail.com>
2025-10-02 20:16:26 +0200

    feat: initial PWA config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,8 @@ import { provideRouter, withViewTransitions } from '@angular/router'
 import { routes } from './app.routes'
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { authCookiesInterceptor } from './intercepters/auth-cookies-interceptor'
+import { provideServiceWorker } from '@angular/service-worker'
+import { environment } from '../environments/environment'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +17,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([authCookiesInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 }
