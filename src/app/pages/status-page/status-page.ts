ce21fb04d83832275cc6faa196ef7cfd97e1fdbd
Andrei Yurkouski <emp74ark@gmail.com>
2025-10-07 13:38:45 +0200

    feat: update the status page

--- src/app/pages/status-page/status-page.ts
+++ src/app/pages/status-page/status-page.ts
@@ -1,15 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core'
 import { HealthStatus } from '../../components/health-status/health-status'
 import { TitleService } from '../../services/title-service'
+import { UpdateButton } from '../../components/update-button/update-button'
+import { AppUpdate } from '../../services/app-update'
+import { AsyncPipe } from '@angular/common'
 
 @Component({
   selector: 'app-status-page',
-  imports: [HealthStatus],
+  imports: [HealthStatus, UpdateButton, AsyncPipe],
   templateUrl: './status-page.html',
   styleUrl: './status-page.css',
 })
 export class StatusPage implements OnInit {
   private readonly titleService = inject(TitleService)
+  private readonly appUpdates = inject(AppUpdate)
+
+  updateFound$ = this.appUpdates.updateFound
 
   ngOnInit() {
     this.titleService.setTitle('User')
