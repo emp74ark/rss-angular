a9a71fb152ea67eb6bc9ffbcebae2f19b05bb7ac
Andrei Yurkouski <emp74ark@gmail.com>
2025-10-08 12:01:19 +0200

    debug: check for the user in AuthGuard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -16,6 +16,7 @@ export const authGuard: CanMatchFn = (route, segments) => {
   const destroyRef = inject(DestroyRef)
 
   if (savedUserId) {
+    console.debug('AuthGuard: user found in local storage')
     return userService.getUser().pipe(
       takeUntilDestroyed(destroyRef),
       catchError((error: HttpErrorResponse) => {
