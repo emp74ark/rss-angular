d3bc7872a3738bf442f86331e69518b92671d505
Andrei Yurkouski <emp74ark@gmail.com>
2025-10-25 11:39:14 +0200

    fix: check if a user is authorized via user service in the public guard

--- src/app/guards/public-guard.ts
+++ src/app/guards/public-guard.ts
@@ -1,14 +1,27 @@
 import { CanActivateFn, Router } from '@angular/router'
-import { inject } from '@angular/core'
+import { DestroyRef, inject } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { catchError, of } from 'rxjs'
+import { UserService } from '../services/user-service'
+import { map } from 'rxjs/operators'
 
 export const publicGuard: CanActivateFn = (route, state) => {
-  const savedUserId = localStorage.getItem('user')
+  const userService = inject(UserService)
+  const destroyRef = inject(DestroyRef)
   const path = route.routeConfig?.path
   const router = inject(Router)
 
-  if (savedUserId && (path === '' || path === 'auth')) {
-    return router.createUrlTree(['/articles'])
-  }
-
-  return true
+  return userService.getUser().pipe(
+    takeUntilDestroyed(destroyRef),
+    catchError(() => {
+      return of(null)
+    }),
+    map((user) => {
+      if (user && (path === '' || path === 'auth')) {
+        return router.createUrlTree(['/articles'])
+      } else {
+        return true
+      }
+    }),
+  )
 }
